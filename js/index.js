7a64880ffa999a7a23b76f5f73e5b5e21ddd8f21
chenxiansheng6 <1921437884@qq.com>
2020-08-06 18:17:41 +0800

    new login

--- js/index.js
+++ js/index.js
@@ -14,5 +14,37 @@ btn.onclick = function() {
     document.documentElement.scrollTop = 0
 };
 
+//登录的一些效果功能
+let loginBtn = document.getElementsByClassName("top_login_link")[0];
+let LoginMethods = document.getElementsByClassName("QQ_Login");
+let LoginBody = document.getElementsByClassName("Login_body")[0];
+let LoginHiddenBody = document.getElementsByClassName("Login_hidden_body")[0];
+let CloseLogin = document.getElementsByClassName("Login_close_btn")[0];
+let Login = document.getElementsByClassName("Login")[0];
+LoginMethods[0].addEventListener("click",function(){
+    this.style.color = `#31c27c`;
+    LoginMethods[1].style.color ="#333";
+    LoginBody.style.display = "block";
+    LoginHiddenBody.style.display = "none";
+},false);
+
+LoginMethods[1].addEventListener("click",function(){
+    LoginMethods[0].style.color ="#333";
+    this.style.color = `#31c27c`;
+    LoginHiddenBody.style.display = "block";
+    LoginBody.style.display = "none";
+
+},false);
+function CloseLoginWindow (){
+    Login.style.display = "none";
+}
+CloseLogin.addEventListener("click",CloseLoginWindow,false);
+
+function openLoginWindow(){
+    Login.style.display = "block";
+
+}
+loginBtn.addEventListener("click",openLoginWindow,false);
+
 
 
