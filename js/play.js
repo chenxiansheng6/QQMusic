038b8d2eb8a5be01cccd8b424327b86d2041dcd7
panxing <1377689310@qq.com>
2020-08-08 16:25:53 +0800

    audio

--- js/play.js
+++ js/play.js
@@ -20,40 +20,49 @@ let pre_btn = function_btn[0];
 let pau_pl_btn = function_btn[1];
 let nex_btn = function_btn[2];
 
-pre_btn.onclick = function (){
-    console.log("ddd");
-};
-
 
 
 let source0 = "D:/git/project/QQMusic/audio/1.mp3";
 let source1 = "D:/git/project/QQMusic/audio/2.mp3";
 let source2 = "D:/git/project/QQMusic/audio/3.mp3";
-   let audio = document.getElementsByTagName("audio")[0];
+let audio = document.getElementsByTagName("audio")[0];
 
-   let source = document.getElementsByTagName("source")[0];
-   console.log(source);
-   console.log(source.src);
-   console.log(source.src);
-
-let aa = 0;
-function played(){
-   console.log("adad");
-
-};
+let source = document.getElementsByTagName("source")[0];
+console.log(source);
 
 
-pau_pl_btn.addEventListener("click", played , false);
 
+let count = 2;
+pau_pl_btn.addEventListener("click",function(){
+    if(audio.paused){
 
-let count = 1;
-nex_btn.addEventListener("click",function(){
-    if(count == 3)
-    {
-        count = 0;
+            audio.play();
+        }
+    else{
+            audio.pause();
     }
-    source.setAttribute("src",source+`${count}`);
-    count ++;
-    console.log(count);
+
 },false);
+
+nex_btn.addEventListener("click",function(){
+    if(count == 4)
+    {
+        count = 1;
+    }
+    audio.setAttribute("src","D:/git/project/QQMusic/audio/"+count+".mp3");
+    audio.load();
+    count ++;
+},false);
+
+pre_btn.addEventListener("click",function(){
+    if(count == 0)
+    {
+        count = 3;
+    }
+    audio.setAttribute("src","D:/git/project/QQMusic/audio/"+count+".mp3");
+    audio.load();
+    count --;
+},false);
+
+
 
